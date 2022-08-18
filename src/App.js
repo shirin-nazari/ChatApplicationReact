1b3b5029e5e609b573bb9e35761bd8f07901279a
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-18 22:11:00 +0430

    remove library

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
-
 import './App.css';
+export default function App() {
+  return (
+    <ChatEngine
+      height="100vh"
+      userName="shirin"
+      userSecret="12341234"
+      projectID="fe679d74-8f68-4657-8737-ec83b62c76f3"
+    />
+  );
+}

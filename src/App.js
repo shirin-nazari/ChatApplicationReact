d91cb076962eaa127d90644855a932fceec888f6
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-18 23:22:53 +0430

    add mui and react 17

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 export default function App() {
   return (
-    <ChatEngine
-      height="100vh"
-      userName="shirin"
-      userSecret="12341234"
-      projectID="fe679d74-8f68-4657-8737-ec83b62c76f3"
-    />
+<h1>
+    hi Iam developer
+    <h2>mewo</h2>
+</h1>
   );
 }

2861fee037d37461bf9cdb92fed83ab37e0b5ffa
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-19 21:01:45 +0430

    add change

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import React from 'react';
 import { SaveAltSharp } from '@material-ui/icons';
 import { LeakRemove } from '@material-ui/icons';
 import './App.css';
-import { ButtonGroup } from '@material-ui/core';
-const CheckBox = () => {
-  return <CheckBox checked />;
-};
-
+import { ButtonGroup, Checkbox } from '@material-ui/core';
+import { useState } from 'react';
+function CheckBoxs() {
+  const [checked, setChecked] = useState(true);
+  return (
+    <Checkbox
+      checked={checked}
+      onChange={(e) => setChecked(e.target.checked)}
+      inputProps={{
+        'aria-label':'secondary checkbox'
+      }}
+    />
+  );
+}
 export default function App() {
   return (
     <>
       {' '}
+      <CheckBoxs />
       <h1>hi Iam developer</h1>
-      <CheckBox />
       <ButtonGroup>
         <Button
           variant="contained"

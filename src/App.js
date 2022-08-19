705dcc502701bb3102e6b22e1a1f6b69d7e616fc
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-19 20:29:41 +0430

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import Button from '@material-ui/core/Button';
 import React from 'react';
-
+import { SaveAltSharp } from '@material-ui/icons';
+import { LeakRemove } from '@material-ui/icons';
 import './App.css';
+import { ButtonGroup } from '@material-ui/core';
+const CheckBox = () => {
+  return <CheckBox checked />;
+};
+
 export default function App() {
   return (
-    <h1>
-      hi Iam developer
-      <Button variant="contained">Shirin</Button>
-    </h1>
+    <>
+      {' '}
+      <h1>hi Iam developer</h1>
+      <CheckBox />
+      <ButtonGroup>
+        <Button
+          variant="contained"
+          color="secondary"
+          // startIcon=
+          endIcon={<SaveAltSharp />}
+          onClick={() => console.log('heeeeeee')}
+        >
+          Shirin
+        </Button>
+        <Button
+          variant="contained"
+          // startIcon=
+          color="primary"
+          endIcon={<LeakRemove />}
+          onClick={() => console.log('heeeeeee')}
+        >
+          bahar
+        </Button>
+      </ButtonGroup>
+    </>
   );
 }

0a90c59280ded39b8357b0de0960f538c79edbea
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-20 22:09:13 +0430

    ):

--- src/App.js
+++ src/App.js
@@ -3,25 +3,60 @@ import React from 'react';
 import { SaveAltSharp } from '@material-ui/icons';
 import { LeakRemove } from '@material-ui/icons';
 import './App.css';
-import { ButtonGroup, Checkbox } from '@material-ui/core';
+import { ButtonGroup, Checkbox, FormControlLabel } from '@material-ui/core';
 import { useState } from 'react';
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+
+const useStyle = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg,#ff6B88,#ff8E53)',
+    // backgroundColor: 'red',
+    margin: 30,
+    border: '0',
+    borderRadius: 15,
+    color: 'white',
+    padding: '0 30px',
+  },
+});
+const theme=createMuiTheme({
+  palette:{
+    primary:{
+      main:
+    }
+  }
+})
+function ButtonStyled() {
+  const classes = useStyle();
+  return <Button className={classes.root}>Test Styles</Button>;
+}
 function CheckBoxs() {
   const [checked, setChecked] = useState(true);
   return (
-    <Checkbox
-      checked={checked}
-      onChange={(e) => setChecked(e.target.checked)}
-      inputProps={{
-        'aria-label':'secondary checkbox'
-      }}
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          inputProps={{
+            'aria-label': 'secondary checkbox',
+          }}
+        />
+      }
+      label="testing checkbox"
     />
   );
 }
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       {' '}
       <CheckBoxs />
+      <h2>hi hi</h2>
+      <ButtonStyled />
       <h1>hi Iam developer</h1>
       <ButtonGroup>
         <Button
@@ -43,6 +78,6 @@ export default function App() {
           bahar
         </Button>
       </ButtonGroup>
-    </>
+    </ThemeProvider>
   );
 }
